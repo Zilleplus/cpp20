839c4dece317e202af44622cfd77f00cebfa3099
Zilleplus <zilleplus@gmail.com>
2022-12-08 07:28:16 +0100

    Added working fibonacci

--- coroutines/fibonacci/hello.cpp
+++ coroutines/fibonacci/hello.cpp
@@ -6,44 +6,35 @@ class ReturnType {
 public:
 	struct promise_type {
 		std::coroutine_handle<promise_type>  get_return_object() { return std::coroutine_handle<promise_type>::from_promise(*this); }
-		int value;
 
-		auto yield_value(auto v) { // co_yield
-			value = v;
-			return std::suspend_always{};
-		}
-
-		void return_void() {} // co_return
-
+		void return_void() {}
 		void unhandled_exception() {}
 
 		std::suspend_always initial_suspend() {
+			// Don't return the first fib number when called upon, just go to sleep.
+			std::cout << "Initial suspend.\n";
 			return {};
 		};
 		std::suspend_always final_suspend() noexcept {
+			std::cout << "Final suspend.\n";
 			return {};
 		}
 	};
 private:
 	std::coroutine_handle<promise_type> hdl;
-
+	
 
 public:
 	bool resume() {
-		if (!hdl)
+		if (!hdl.done())
 		{
-			return false;
+			hdl.resume();
 		}
-		hdl.resume();
 
-		return true;
+		return hdl.done();
 	}
 
-	int value() const {
-		return hdl.promise().value;
-	}
-
-	ReturnType(auto h) : hdl(h) {}
+	ReturnType(auto h): hdl(h){}
 
 	ReturnType(const ReturnType& other) = delete;
 	ReturnType operator=(const ReturnType& other) = delete;
@@ -54,38 +45,15 @@ public:
 	}
 };
 
-ReturnType Hello(int n)
+ReturnType Hello()
 {
-	if (n < 1)
-	{
-		co_return;
-	}
-	co_yield 1;
-
-	if (n < 2)
-	{
-		co_return;
-	}
-	co_yield 1;
-
-	int current = 1;
-	int previous = 1;
-	for (int i = 0; i < n - 2; ++i)
-	{
-		auto new_value = current + previous;
-		co_yield new_value;
-		previous = current;
-		current = new_value;
-	}
+	std::cout << "hello world" << std::endl;
+	co_await std::suspend_always(); // does not work either
 }
 
 int main() {
-	auto series = Hello(10);
-	auto blah = series.resume();
-	while (series.resume())
-	{
-		std::cout << series.value() << "\n";
-	}
+	auto series = Hello();
+	series.resume();
 
 	return 0;
 }
